8bd9d20929de1e42654fde6af117617ebe094dc0
SecurityEasy <seasycdmx@gmail.com>
2025-06-02 12:26:05 -0600

    script.js

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ let angle = 195; // Comienza apuntando a otro premio visualmente
 let isSpinning = false;
 
 function findAgle() {
-  const fixedIndex = premios.findIndex((p) => p.includes("1 Renovación"));
+  const fixedIndex = premios.findIndex((p) => p.includes("1 VL103M + 10 SIM Telcel");
   const degreesPerPrize = 360 / premios.length;
   const pointerOffset = -degreesPerPrize; // 🔺 Donde apunta el fueguito (arriba)
   const targetAngle =
